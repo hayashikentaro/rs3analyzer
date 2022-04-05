978a09342453fcfbba6152e6819ecb414ffd40c6
hayashikentarou <jacques.n>
2022-04-06 06:21:13 +0900

    rename

--- util.js
+++ util.js
@@ -1,14 +1,14 @@
 import * as fs from 'fs';
 
 
-const print = (content, offset, out) => {
+const output = (content, offset, converter) => {
     for (let i = 0; i < 100; i++) {
-        console.log((offset + i).toString(16) + " : " + out(content, offset + i));
+        console.log((offset + i).toString(16) + " : " + converter(content, offset + i));
     }
 };
 
-export const dump = (offset, out) => {
+export const dump = (offset, converter) => {
     fs.readFile('rom', (err, content) => {
-        return print(content, offset, out);
+        return output(content, offset, converter);
     })
 };
