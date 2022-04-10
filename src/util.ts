e957c4ea8341b8824c64299648fa8d11967e967e
hayashikentarou <jacques.n>
2022-04-10 21:28:00 +0900

    remove redundant description

--- src/util.ts
+++ src/util.ts
@@ -6,6 +6,6 @@ type Converter = (buf: Byte[]) => void;
 export const dump: (offset :number, converter :Converter) => void =
   (offset, converter) => {
     fs.readFile('in', (err: NodeJS.ErrnoException | null, content: Buffer) => {
-        converter(Array.from(new Uint8Array(content)).slice(offset).map((num) => createByte(num)));
+        converter(Array.from(content).slice(offset).map((num) => createByte(num)));
     });
 }
