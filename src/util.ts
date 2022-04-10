12911f9d244fb175c3c99987a2eefa1f802b5b0b
hayashikentarou <jacques.n>
2022-04-10 21:29:30 +0900

    remove redundant type definition

--- src/util.ts
+++ src/util.ts
@@ -1,9 +1,7 @@
 import * as fs from 'fs';
 import {Byte, createByte} from "./byte";
 
-type Converter = (buf: Byte[]) => void;
-
-export const dump: (offset :number, converter :Converter) => void =
+export const dump: (offset :number, converter :(buf: Byte[]) => void) => void =
   (offset, converter) => {
     fs.readFile('in', (err, content) => {
         converter(Array.from(content).slice(offset).map((num) => createByte(num)));
