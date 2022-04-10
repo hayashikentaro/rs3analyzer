70f928cde5ec85333c0afe69bd39ec04acf2cd5c
hayashikentarou <jacques.n>
2022-04-11 02:24:45 +0900

    organize indent

--- src/byte.ts
+++ src/byte.ts
@@ -2,24 +2,24 @@
 export const bitPerByte = 8;
 
 export interface Byte {
-  value: number;
-  bit: (index :number) => number;
+    value: number;
+    bit: (index :number) => number;
 }
 
 export const createByte: (raw: number) => Byte = (raw) => {
-  return {
-    value: raw & 0xFF,
-    bit: function(index: number): number {
-      return takeBit(this.value, index);
-    },
-  }
+    return {
+        value: raw & 0xFF,
+        bit: function(index: number): number {
+            return takeBit(this.value, index);
+        },
+    }
 }
 
 // リトルエンディアン（小さい方から）
 const takeBit: (byte :number, digit: number) => number = (byte, digit) => {
-  return (byte >> (bitPerByte - 1 - digit)) & 0x01;
+    return (byte >> (bitPerByte - 1 - digit)) & 0x01;
 }
 
 export const bytesToBuffer :(bytes :Byte[]) => ArrayBuffer = (bytes) => {
-  return new Uint8Array(bytes.map((byte) => byte.value)).buffer;
+    return new Uint8Array(bytes.map((byte) => byte.value)).buffer;
 }
--- src/util.ts
+++ src/util.ts
@@ -3,9 +3,9 @@ import {Byte, createByte} from "./byte";
 
 export const dump: (offset :number, converter :(buf: Byte[]) => void) => void =
   (offset, converter) => {
-    fs.readFile('in', (err, content) => {
-        converter(Array.from(content).slice(offset).map((num) => createByte(num)));
-    });
+      fs.readFile('in', (err, content) => {
+          converter(Array.from(content).slice(offset).map((num) => createByte(num)));
+      });
 }
 
 export const writeFile: (fileName: string, buffer: ArrayBuffer) => void = (fileName, buffer) => {
